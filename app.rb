8af1a4f3585ae140c9ac7e32ba6b4aef5791c957
phbord <phbord@gmail.com>
2021-04-26 16:26:08 +0200

    first commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'dotenv'
+require 'pry'
+require 'rspec'
+require 'rubocop'
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/page_1'
+
+Dotenv.load('.env')
+
+#binding.pry
+
---
+++ spec/page_1_spec.rb
@@ -0,0 +1,2 @@
+require_relative '../lib/page_1.rb'
+
